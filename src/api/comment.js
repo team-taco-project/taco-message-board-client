f2b50c59804a625ca0b9a662ef6daa4b14883f93
naguiluz <nicolasaguiluz2@gmail.com>
2021-08-26 09:28:06 -0500

    small changes to comment api

--- src/api/comment.js
+++ src/api/comment.js
@@ -16,48 +16,48 @@ export const createComment = (data, user, id) => {
   })
 }
 
-// Index request
-// no data, we will need a token
-export const indexPosts = (user) => {
-  return axios({
-    // method key sets the HTTP verb/method for this request
-    // GET is the default method, so we can include or not up to us
-    method: 'GET',
-    url: apiUrl + '/posts',
-    headers: {
-      Authorization: `Bearer ${user.token}`
-    }
-  })
-}
-export const indexAllPosts = (user) => {
-  return axios({
-    // method key sets the HTTP verb/method for this request
-    // GET is the default method, so we can include or not up to us
-    method: 'GET',
-    url: apiUrl + '/posts-all'
-  })
-}
-
-// // GET /Posts/:id
-// export const showPost = (id, user) => {
+// // Index request
+// // no data, we will need a token
+// export const indexPosts = (user) => {
 //   return axios({
-//     url: apiUrl + '/post/' + id,
-//     // method is optional, default is GET
+//     // method key sets the HTTP verb/method for this request
+//     // GET is the default method, so we can include or not up to us
+//     method: 'GET',
+//     url: apiUrl + '/posts',
 //     headers: {
 //       Authorization: `Bearer ${user.token}`
 //     }
 //   })
 // }
-
-// GET /Post no id
-// export const showPost = (id) => {
+// export const indexAllPosts = (user) => {
 //   return axios({
-//     url: apiUrl + '/post/' + id
+//     // method key sets the HTTP verb/method for this request
+//     // GET is the default method, so we can include or not up to us
+//     method: 'GET',
+//     url: apiUrl + '/posts-all'
 //   })
 // }
 
+// // // GET /Posts/:id
+// // export const showPost = (id, user) => {
+// //   return axios({
+// //     url: apiUrl + '/post/' + id,
+// //     // method is optional, default is GET
+// //     headers: {
+// //       Authorization: `Bearer ${user.token}`
+// //     }
+// //   })
+// // }
+
+// // GET /Post no id
+// // export const showPost = (id) => {
+// //   return axios({
+// //     url: apiUrl + '/post/' + id
+// //   })
+// // }
+
 // DELETE /Posts/:id
-export const deletePost = (id, user) => {
+export const deleteComment = (id, user) => {
   return axios({
     url: apiUrl + '/post/' + id,
     method: 'DELETE',
