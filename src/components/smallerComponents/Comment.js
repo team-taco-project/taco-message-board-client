0ce310bba1e06f53b3cce78acea26764d95111d2
naguiluz <nicolasaguiluz2@gmail.com>
2021-08-26 12:20:28 -0500

    Updated ShowPost Comment and Post
    updated flow of props through the files

--- src/components/smallerComponents/Comment.js
+++ src/components/smallerComponents/Comment.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 // creating a component comment that accepts props it will be passed
 
-const Comment = (props, { handleDeleteComment }) => (
+const Comment = (props) => (
   <ul>
     <li>Comment: {props.text}</li>
     <li>Image: {props.image}</li>
-    <Button onClick={handleDeleteComment}>Delete</Button>
+    {/* here Comment is accepting onClick from props */}
+    <Button onClick={props.onClick}>Delete</Button>
   </ul>
 )
 
